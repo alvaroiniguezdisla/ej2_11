a074367a6e995eac4f4223bfa9c8222410f300f3
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:47:23 +0100

    ej11 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_11;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[]lista1= {1,2,3};
+        int[]lista2= {2,4,6};
+        System.out.println(ej2_11.pescalar(lista1,lista2,lista1.length-1));
+
+
+    }
+
 }
--- src/dominio/ej2_11.java
+++ src/dominio/ej2_11.java
@@ -1,2 +1,11 @@
-package dominio;public class ej2_11 {
+package dominio;
+
+public class ej2_11 {
+    public static int pescalar(int[]lista1,int[]lista2,int i) {
+        if(i<0) {
+            return 0;
+        }else {
+            return (lista1[i]*lista2[i])+pescalar(lista1,lista2,i-1);
+        }
+    }
 }
